307f40db24d846607302fa724cd4914695ed49c7
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2021-07-23 19:40:58 +0700

    add deck methods

--- src/js/Object/Deck.js
+++ src/js/Object/Deck.js
@@ -1,11 +1,8 @@
 import Card from './Card';
-// import Image from '../Loader/Image';
-
-let height = window.innerHeight;
-let width = window.innerWidth;
-
-const CARD_TYPES = ['C', 'D', 'H', 'S'];
-const CARD_NUMBERS = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2'];
+import Image from '../Loader/Image';
+import { DECK_POSITION, PLAYER_POSITION } from '../Reference/Position';
+import Phaser from 'phaser';
+import { CARD_TYPES, CARD_NUMBERS, MAX_PLAYER_CARD, SCALE_IMAGE, SPACE, DURATION } from '../Constant/Config';
 
 export default class Deck {
     constructor() {
@@ -16,26 +13,104 @@ export default class Deck {
             for (const number of CARD_NUMBERS) {
                 let card = new Card(`${number}${type}`, type, number, value)
                 cards.push(card);
-                // scene.load.image(`${number}${type}`, Image[`./${number}${type}.png`]);
                 value += 1;
             }
         }
         this.cards = cards;
-        // this.scene = scene;
     }
 
-    // render() {
-    //     for (const card of this.cards) {
-    //         this.scene.make.sprite({
-    //             x: width / 2,
-    //             y: height / 2,
-    //             key: `${card.key}`,
-    //             add: true,
-    //             scale: {
-    //                 x: 0.2,
-    //                 y: 0.2,
-    //             },
-    //         });
-    //     }
-    // }
+    getCards() {
+        return this.cards;
+    }
+
+    clearCards() {
+        this.cards = [];
+    }
+
+    loadImage(scene) {
+        scene.load.image('back', Image['./red_back.png']);
+        this.cards.forEach(card => {
+            scene.load.image(card.key, Image[`./${card.key}.png`]);
+        });
+    }
+
+    render(scene) {
+        this.cards.forEach(card => {
+            let card_sprite = scene.make.sprite({
+                x: DECK_POSITION.x,
+                y: DECK_POSITION.y,
+                key: 'back',
+                add: true,
+                scale: {
+                    x: SCALE_IMAGE,
+                    y: SCALE_IMAGE,
+                },
+            });
+            card.sprite = card_sprite;
+        });
+    }
+
+    shuffle() {
+        Phaser.Actions.Shuffle(this.cards);
+        this.cards.forEach((card, index) => {
+            card.sprite.setDepth(index);
+        })
+    }
+
+    giveCard(scene) {
+        for (let player = 0; player < scene.state.players.length; player++) {
+            scene.state.players[player].setCards(this.cards.slice(player * MAX_PLAYER_CARD, (player + 1) * MAX_PLAYER_CARD));
+        }
+        let sprites = this.cards.map(card => card.sprite);
+        this.cards = [];
+        return scene.tweens.add({
+            targets: sprites,
+            x: function (target, key, value, targetIndex, totalTargets, tween) {
+                if (targetIndex >= 0 * MAX_PLAYER_CARD && targetIndex < 1 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[0].x + (targetIndex % MAX_PLAYER_CARD - Math.floor(MAX_PLAYER_CARD / 2)) * SPACE;
+                }
+                else if (targetIndex >= 1 * MAX_PLAYER_CARD && targetIndex < 2 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[1].x;
+                }
+                else if (targetIndex >= 2 * MAX_PLAYER_CARD && targetIndex < 3 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[2].x - (targetIndex % MAX_PLAYER_CARD - Math.floor(MAX_PLAYER_CARD / 2)) * SPACE;
+                }
+                else if (targetIndex >= 3 * MAX_PLAYER_CARD && targetIndex < 4 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[3].x;
+                }
+            },
+            y: function (target, key, value, targetIndex, totalTargets, tween) {
+                if (targetIndex >= 0 * MAX_PLAYER_CARD && targetIndex < 1 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[0].y;
+                }
+                else if (targetIndex >= 1 * MAX_PLAYER_CARD && targetIndex < 2 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[1].y + (targetIndex % MAX_PLAYER_CARD - Math.floor(MAX_PLAYER_CARD / 2)) * SPACE;
+                }
+                else if (targetIndex >= 2 * MAX_PLAYER_CARD && targetIndex < 3 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[2].y 
+                }
+                else if (targetIndex >= 3 * MAX_PLAYER_CARD && targetIndex < 4 * MAX_PLAYER_CARD) {
+                    return PLAYER_POSITION[3].y - (targetIndex % MAX_PLAYER_CARD - Math.floor(MAX_PLAYER_CARD / 2)) * SPACE;
+                }
+            },
+            angle: function (target, key, value, targetIndex, totalTargets, tween) {
+                if (targetIndex >= 0 * MAX_PLAYER_CARD && targetIndex < 1 * MAX_PLAYER_CARD) {
+                    return 0;
+                }
+                else if (targetIndex >= 1 * MAX_PLAYER_CARD && targetIndex < 2 * MAX_PLAYER_CARD) {
+                    return 90;
+                }
+                else if (targetIndex >= 2 * MAX_PLAYER_CARD && targetIndex < 3 * MAX_PLAYER_CARD) {
+                    return 0; 
+                }
+                else if (targetIndex >= 3 * MAX_PLAYER_CARD && targetIndex < 4 * MAX_PLAYER_CARD) {
+                    return 90;
+                }
+            },
+            duration: DURATION,
+            delay: function (target, key, value, targetIndex, totalTargets, tween) {
+                return targetIndex * DURATION;
+            }
+        })
+    }
 }
