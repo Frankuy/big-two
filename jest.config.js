9afadd81d3d5ecae82a554dc00b9fd14e2c43c76
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2021-07-24 12:44:30 +0700

    add jest as test library

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    verbose: true,
+    testEnvironment: "jsdom",
+    setupFiles: ['./setupTests.js']
+    // setupFiles: ["jest-canvas-mock"]
+}
---
+++ setupTests.js
@@ -0,0 +1,67 @@
+import "jest-canvas-mock";
+import Card from './src/js/Object/Card';
+
+export const diamond = new Card('3D', 'D', '3', 1);
+export const spades = new Card('3S', 'S', '3', 1);
+export const hearts = new Card('3H', 'H', '3', 1);
+export const clubs = new Card('3C', 'C', '3', 1);
+
+export const four_diamond = new Card('4D', 'D', '4', 2);
+export const four_spades = new Card('4S', 'S', '4', 2);
+export const four_hearts = new Card('4H', 'H', '4', 2);
+export const four_clubs = new Card('4C', 'C', '4', 2);
+
+export const five_diamond = new Card('5D', 'D', '5', 3);
+export const five_spades = new Card('5S', 'S', '5', 3);
+export const five_hearts = new Card('5H', 'H', '5', 3);
+export const five_clubs = new Card('5C', 'C', '5', 3);
+
+export const six_diamond = new Card('6D', 'D', '6', 4);
+export const six_spades = new Card('6S', 'S', '6', 4);
+export const six_hearts = new Card('6H', 'H', '6', 4);
+export const six_clubs = new Card('6C', 'C', '6', 4);
+
+export const seven_diamond = new Card('7D', 'D', '7', 5);
+export const seven_spades = new Card('7S', 'S', '7', 5);
+export const seven_hearts = new Card('7H', 'H', '7', 5);
+export const seven_clubs = new Card('7C', 'C', '7', 5);
+
+export const eight_diamond = new Card('8D', 'D', '8', 6);
+export const eight_spades = new Card('8S', 'S', '8', 6);
+export const eight_hearts = new Card('8H', 'H', '8', 6);
+export const eight_clubs = new Card('8C', 'C', '8', 6);
+
+export const nine_diamond = new Card('9D', 'D', '9', 7);
+export const nine_spades = new Card('9S', 'S', '9', 7);
+export const nine_hearts = new Card('9H', 'H', '9', 7);
+export const nine_clubs = new Card('9C', 'C', '9', 7);
+
+export const ten_diamond = new Card('10D', 'D', '10', 8);
+export const ten_spades = new Card('10S', 'S', '10', 8);
+export const ten_hearts = new Card('10H', 'H', '10', 8);
+export const ten_clubs = new Card('10C', 'C', '10', 8);
+
+export const jack_diamond = new Card('JD', 'D', 'J', 9);
+export const jack_spades = new Card('JS', 'S', 'J', 9);
+export const jack_hearts = new Card('JH', 'H', 'J', 9);
+export const jack_clubs = new Card('JC', 'C', 'J', 9);
+
+export const queen_diamond = new Card('QD', 'D', 'Q', 10);
+export const queen_spades = new Card('QS', 'S', 'Q', 10);
+export const queen_hearts = new Card('QH', 'H', 'Q', 10);
+export const queen_clubs = new Card('QC', 'C', 'Q', 10);
+
+export const king_diamond = new Card('KD', 'D', 'K', 11);
+export const king_spades = new Card('KS', 'S', 'K', 11);
+export const king_hearts = new Card('KH', 'H', 'K', 11);
+export const king_clubs = new Card('KC', 'C', 'K', 11);
+
+export const as_diamond = new Card('AD', 'D', 'A', 12);
+export const as_spades = new Card('AS', 'S', 'A', 12);
+export const as_hearts = new Card('AH', 'H', 'A', 12);
+export const as_clubs = new Card('AC', 'C', 'A', 12);
+
+export const two_diamond = new Card('2D', 'D', '2', 13);
+export const two_spades = new Card('2S', 'S', '2', 13);
+export const two_hearts = new Card('2H', 'H', '2', 13);
+export const two_clubs = new Card('2C', 'C', '2', 13);
